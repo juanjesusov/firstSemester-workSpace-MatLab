7e448d9e75e6d242c8eb66bdd8fa31ad4e81de86
Juan Jesús Ortiz Vazquez <juanjesusov@gmail.com>
2021-09-28 07:30:58 -0500

    condicional if

---
+++ condicionalIf.m
@@ -0,0 +1,8 @@
+% Juan Jesús Ortiz Vazquez
+semana=["lunes","martes","miercoles","jueves","viernes","sabado","domingo"];
+dia = semana(randi(7));
+if dia=="lunes" | dia=="martes" | dia=="jueves"
+    tarea="si";
+else
+    tarea="no";
+end
